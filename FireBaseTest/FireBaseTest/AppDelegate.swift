bb1a7ef564febf96907acdec1b37d438e6cb4db8
Benedikt Langer <S1810237024@students.fh-hagenberg.at>
2020-06-14 14:58:27 +0200

    Cloud Functions Ergebnis 14.06.20

---
+++ FireBaseTest/FireBaseTest/AppDelegate.swift
@@ -0,0 +1,243 @@
+//
+//  AppDelegate.swift
+//  FireBaseTest
+//
+//  Created by Benedikt Langer on 24.05.20.
+//  Copyright © 2020 Benedikt. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
+import UserNotifications
+
+
+var data: [String: AnyObject]?
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+   
+    let gcmMessageIDKey = "gcm.message_id"
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+       
+        FirebaseApp.configure()
+       let db = Firestore.firestore()
+       print(db)
+       
+       Messaging.messaging().delegate = self
+               
+        
+        if #available(iOS 10.0, *) {
+          // For iOS 10 display notification (sent via APNS)
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+        } else {
+          let settings: UIUserNotificationSettings =
+          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
+        
+        let notificationOption = launchOptions?[.remoteNotification]
+        
+        if let notification = notificationOption as? [String: AnyObject]{
+            
+            print(notification)
+        }
+        
+       
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "FireBaseTest")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    // [START receive_message]
+      func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+          print("Message ID: \(messageID)")
+        }
+
+        // Print full message.
+        print(userInfo)
+      }
+    
+    
+      func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                       fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey]{
+          print("Message ID: \(messageID)")
+            
+        }
+        
+        guard let aps = userInfo["aps"] as? [String: AnyObject] else {
+          completionHandler(.failed)
+          return
+        }
+
+        // Print full message.
+        print(aps)
+
+        completionHandler(UIBackgroundFetchResult.newData)
+      }
+      // [END receive_message]
+      func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Unable to register for remote notifications: \(error.localizedDescription)")
+      }
+
+      // This function is added here only for debugging purposes, and can be removed if swizzling is enabled.
+      // If swizzling is disabled then this function must be implemented so that the APNs token can be paired to
+      // the FCM registration token.
+      func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNs token retrieved: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+        // With swizzling disabled you must set the APNs token here.
+        // Messaging.messaging().apnsToken = deviceToken
+      }
+    
+
+}
+
+@available(iOS 10, *)
+extension AppDelegate: UNUserNotificationCenterDelegate{
+    // Receive displayed notifications for iOS 10 devices.
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+      withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+      let userInfo = notification.request.content.userInfo
+
+      // With swizzling disabled you must let Messaging know about the message, for Analytics
+      Messaging.messaging().appDidReceiveMessage(userInfo)
+      // Print message ID.
+      if let messageID = userInfo[gcmMessageIDKey] {
+        print("Message ID: \(messageID)")
+      }
+
+      // Print full message.
+      print(userInfo)
+        data = userInfo["data"] as! [String: AnyObject]
+        
+        NotificationCenter.default.post(name: .didReceiveData, object: nil, userInfo: data)
+
+      // Change this to your preferred presentation option
+      completionHandler([[.alert, .sound]])
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+      let userInfo = response.notification.request.content.userInfo
+      // Print message ID.
+      if let messageID = userInfo[gcmMessageIDKey] {
+        print("Message ID: \(messageID)")
+      }
+
+      // Print full message.
+      print(userInfo)
+        
+        data = userInfo["data"] as! [String: AnyObject]
+        
+       NotificationCenter.default.post(name: .didReceiveData, object: nil, userInfo: data)
+        
+
+      completionHandler()
+    }
+}
+
+
+extension AppDelegate: MessagingDelegate{
+    
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+      print("Firebase registration token: \(fcmToken)")
+
+      let dataDict:[String: String] = ["token": fcmToken]
+      NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+}
+
+extension Notification.Name {
+static let didReceiveData = Notification.Name("didReceiveData")
+static let didCompleteTask = Notification.Name("didCompleteTask")
+static let completedLengthyDownload = Notification.Name("completedLengthyDownload")
+}
---
+++ FireBaseTest/FireBaseTest/ViewController.swift
@@ -0,0 +1,220 @@
+//
+//  ViewController.swift
+//  FireBaseTest
+//
+//  Created by Benedikt Langer on 24.05.20.
+//  Copyright © 2020 Benedikt. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseFunctions
+import UserNotifications
+
+var dbstat: Firestore!
+
+class ViewController: UIViewController {
+    
+    var db: Firestore!
+    var ref: DocumentReference?
+    var refID: String = "gxakLA319khxbfwMhr6Q"
+    lazy var functions = Functions.functions()
+    var userID: String = ""
+    var firstUser: DocumentReference?
+    var token: String?
+    static var receivedAps: [String: AnyObject]?
+    var inc: Int = 0;
+    
+    static func setAps(aps: [String: AnyObject]){
+       receivedAps  = aps
+    }
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // [START setup]
+        let settings = FirestoreSettings()
+
+        Firestore.firestore().settings = settings
+        // [END setup]
+        db = Firestore.firestore()
+        
+        InstanceID.instanceID().instanceID { (result, error) in
+            if let error = error{
+                print(error.localizedDescription)
+            }else if let result = result{
+                self.token = result.token
+                //print(result.token)
+            }
+        }
+        label.text = "\(ViewController.receivedAps?.keys)"
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(onDidReceiveData(_:)), name: .didReceiveData, object: nil)
+        
+    }
+    
+    @objc func onDidReceiveData(_ notification:Notification) {
+        // Do something now
+        
+        if let data = notification.userInfo as? [String: AnyObject]
+        {
+            let count = data["customerCount"] as? Int
+            let firstName = data["first"] as? String
+            let firstPosition = data["numberFirst"] as? Int
+            let secondName = data["second"] as? String
+            label.text = "Total Customers: \(count ?? 0), first: \(firstName ?? "")\n" +
+            "FirstPosition: \(firstPosition ?? 0), SecondName: \(secondName ?? ""))"
+        }
+    }
+    
+    
+    @IBAction func createQueue(_ sender: UIButton) {
+        
+        print("Token: \(token)" )
+        
+        
+        ref = db.collection("queue").addDocument(data: [
+            "DeviceToken": token ?? "",
+            "Name": "Queue4",
+            "Reminder": 3,
+            "TimePerCustomer": 5,
+            "UserQueue" : [DocumentReference]()
+        ]){error in
+            if let error = error{
+                print("Error adding document: \(error)")
+            }else{
+                print("Document added with ID: \(self.ref!.documentID)")
+            }
+        }
+        db.collection("queue").document(ref!.documentID).addSnapshotListener { (documentSnapshot, error) in
+            guard let document = documentSnapshot else {
+                print("Error fetching document: \(error!)")
+                return
+            }
+            guard let data = document.data() else {
+                print("Document data was empty")
+                return
+            }
+            let referenceArray = data["UserQueue"] as! [DocumentReference]
+            var totalTime = data["TimePerCustomer"] as! Int
+            totalTime = totalTime * referenceArray.count
+
+            if !referenceArray.isEmpty{
+                self.firstUser = referenceArray[0]
+                self.getUser(ref: self.firstUser!, totalTime: totalTime, count: referenceArray.count)
+            }
+            
+            
+            
+        }
+        //refID = ref?.documentID
+        
+    }
+    
+    func getUser(ref: DocumentReference, totalTime: Int, count: Int){
+        
+        ref.getDocument { (document, error) in
+            guard let document = document else{
+                print(error?.localizedDescription)
+                return
+            }
+            if let name = document.data()?["Name"] as? String {
+                print("Total Time: \(totalTime); First Customer: \(name)")
+                print("Total in queue: \(count)")
+            }else{
+                self.getUser(ref: ref, totalTime: totalTime, count: count)
+            }
+        }
+    }
+    
+    @IBAction func deleteActualQueue(_ sender: UIButton) {
+        
+        db.collection("queue").document(refID).delete(){err in
+            if let err = err {
+                print("Error removing document: \(err)")
+            } else {
+                print("Document successfully removed!")
+            }
+        }
+    }
+    
+    @IBAction func addUser(_ sender: UIButton) {
+        
+        let users = ["User1", "User2", "User3", "User4", "User5"]
+        
+        
+        let dataDict: [String: Any] = ["id": refID, "reference": users[inc]]
+        inc += 1
+        
+        functions.httpsCallable("putReference").call(dataDict) { (result, error) in
+            if let error = error as NSError? {
+              if error.domain == FunctionsErrorDomain {
+                let code = FunctionsErrorCode(rawValue: error.code)
+                let message = error.localizedDescription
+                let details = error.userInfo[FunctionsErrorDetailsKey]
+                print(message, details)
+              }
+              // ...
+            }
+            
+        }
+    }
+    
+    @IBAction func deleteFirstUser(_ sender: UIButton) {
+        
+        
+        let call: Void = db.collection("queue").document(refID).updateData(["UserQueue" : FieldValue.arrayRemove([firstUser])])
+        
+    
+}
+    
+    @IBAction func retrieveData(_ sender: UIButton) {
+        
+        let refDict = ["queueID": refID, "userID": "User3"]
+        
+        functions.httpsCallable("fetchQueueData").call(refDict) { (result, error) in
+            
+            guard let data = result?.data as? [String: Any] else{
+                return
+            }
+            
+            if let total = data["TotalNumber"] as? Int, let totalTime = data["TotalTime"] as? Int{
+                print("There are \(total) user in the line. Total Time is \(totalTime) minutes")
+            }
+            
+            if let index = data["CurrentIndex"] as? Int {
+                print("Your position is \(index+1) in the queue. There are \(index) people ahead")
+            }
+            
+            if let error = error{
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+
+    
+//public struct Queue: Codable {
+//    
+//    var Name: String?
+//    var Reminder: Int64?
+//    var TimePerCustomer: Int64?
+//    var UserQueue: [DocumentReference]
+//    
+//    enum CodingKeys: String, CodingKey{
+//
+//        case Name
+//        case Reminder
+//        case TimePerCustomer
+//        case UserQueue
+//    }
+//}
+
+
+
+}
+
+
